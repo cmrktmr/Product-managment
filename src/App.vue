d47df1ddd6f22bb2144796d8f5818fa5d310c243
Cemre Aktemur <cemre_aktemur@hotmail.com>
2021-04-08 00:08:17 +0300

    route added

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <app-Header></app-Header>
+    <app-Footer></app-Footer>
+   <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from "./components/shared/Footer";
+import Footer from "./components/shared/Header";
+
+
+
+export default {
+  components: {
+    appHeader: Header,
+    appFooter: Footer,
+  },
+};
+</script>
+
+<style>
+</style>
