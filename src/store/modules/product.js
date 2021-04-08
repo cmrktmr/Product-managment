229f1b06cc77ebb51ef9f26341a59feb82140be3
Cemre Aktemur <cemre_aktemur@hotmail.com>
2021-04-08 23:19:06 +0300

    product added

--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -1,3 +1,5 @@
+import Vue from "vue";
+
 const state = {
   products: []
 };
@@ -20,13 +22,28 @@ const actions = {
   initApp({ commit }) {
     //vue Resource işlemleri
   },
-  saveProduct({ commit }, payload) {
-    //vue Resource işlemleri
+  saveProduct({ dispatch,commit,state },product) {
+    Vue.http.post(
+      "https://urun-islemleri-6d2fd-default-rtdb.firebaseio.com/products.json",product )
+      .then( (response) => {
+        /****ürün listesinin güncellenmesi*****/ 
+        product.key= response.body.name;
+        commit("updateProductList",product)
+        /*******/
+        let tradeResult={
+          purchase:product.price,
+          sale:0,
+          count:product.count
+        }
+        dispatch("setTradeResult", tradeResult)
+      } )
   },
   sellProduct({ commit }, payload) {
     //vue Resource işlemleri
   }
 };
+
+
 
 export default {
   state,
--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,23 @@
 import Vuex from "vuex";
 import Vue from "vue";
-import  product from "./modules/product"
+import product from "./modules/product";
+import * as getters from "./getters"
+import * as mutations from "./mutations"
+import * as actions from "./actions"
 
+Vue.use(Vuex);
 
-Vue.use(vuex);
+export const store = new Vuex.Store({
+  state: {
+    purchase: 0.0,
+    sale: 0.0,
+    balance: 0.0
+  },
+  getters,
+  mutations,
+  actions,
 
-export const store = new Vuex.store({
-    state:{
-
-    },
-    modules:{
-        product
-    }
-})
+  modules: {
+    product
+  }
+});
